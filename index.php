0b70207921361c9816a7b0c2f0ea8a8c108b8566
waltroou <walter.roou.21@cnu.edu>
2025-12-03 11:32:09 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include 'connect.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>QueryCrew</title>
+</head>
+<body>
+    <h2>Welcome to QueryCrew</h2>
+    <p>PHP file created directly on GitHub </p>
+</body>
+</html>
