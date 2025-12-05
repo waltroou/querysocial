505c24f72b862d0bc6ad2fe0b9dfcf4990b51962
TotalDr3w <88339779+TotalDr3w@users.noreply.github.com>
2025-12-05 15:19:40 -0500

    Updated connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";           // VM MySQL
-$dbname = "social_platform";   // the DB you already created
-$username = "appuser";
-$password = "StrongPassword123!";
+$host = "cpsc336-fa25-proj.c8iy1t7vl9pp.us-east-1.rds.amazonaws.com";           // VM MySQL
+$dbname = "querycrew";   // the DB you already created
+$username = "querycrew";
+$password = "336FA25querycrew";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
